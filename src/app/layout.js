a52648f6a32ccb4fbf98ad9f505d9a38d0f9332d
Sanskar singh <sanskarsingh@Sanskars-MacBook-Air.local>
2026-05-02 15:06:18 +0530

    Fix deployment and jobs list issues

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Job-Chain",
@@ -21,9 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="antialiased">
         <div className="h-screen overflow-auto">
           <Navbar />
           {children}
